d1add3b90f15c86d5bebc92420830cbd7a0432a8
User <user@Users-MacBook-Air.local>
2024-10-16 15:52:46 +0530

    update on 16-10-24

--- ios/PatientAppTests/PatientAppTests.m
+++ ios/PatientAppTests/PatientAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface patientappTests : XCTestCase
+@interface PatientAppTests : XCTestCase
 
 @end
 
-@implementation patientappTests
+@implementation PatientAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
