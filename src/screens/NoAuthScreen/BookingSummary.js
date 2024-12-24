65d831f1d63e1fe9bbbf221d991dbcb1950ddbd8
SUBHAJIT GHOSH <subhajitghosh@SUBHAJITs-Mac-mini.local>
2024-12-24 14:23:48 +0530

    update on 24-12-24

--- src/screens/NoAuthScreen/BookingSummary.js
+++ src/screens/NoAuthScreen/BookingSummary.js
@@ -241,7 +241,9 @@ const BookingSummary = ({ navigation, route }) => {
                         //     { text: 'OK', onPress: () => navigation.navigate('ThankYouBookingScreen', { detailsData: JSON.stringify(res.data.data) }) },
                         // ]);
                         logPurchaseEvent(payableAmount)
-                        logPurchaseEventGoogle(payableAmount, transactionId)
+                        if(Platform.OS === 'ios'){
+                            logPurchaseEventGoogle(payableAmount, transactionId)
+                        }
                         navigation.navigate('ThankYouBookingScreen', { detailsData: JSON.stringify(res.data.data) })
                     } else {
                         //console.log('not ok');
